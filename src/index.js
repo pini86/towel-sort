2d1de31246b73795a1c758d3825e4f985609d018
Mikhail Druzhinin <pini86@mail.ru>
2022-01-31 16:27:09 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  let outArray = [];
+
+  if (!matrix || !matrix.length){
+    return outArray
+  } 
+
+  function sort (tempRow, current, numRow) {
+    return tempRow.concat( ( ( numRow % 2 ) ? current.reverse() : current ) );//если строка нечетная - в обратном порядке, иначе - копируем её как есть
+  }
+
+  outArray = matrix.reduce( sort , [] );
+
+  return outArray;
 }
